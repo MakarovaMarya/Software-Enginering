824016ad02af717062ea498707fb37fc8dbca081
MakarovaMarya <161197094+MakarovaMarya@users.noreply.github.com>
2025-11-07 10:25:20 +0500

    Update code9.py

--- code9.py
+++ code9.py
@@ -1,1 +1,17 @@
+class Maria:
+    __slots__ = ['name']
+
+    def __init__(self, name):
+        if name == 'Мария':
+            self.name = f"Да, Я {name}"
+        else:
+            self.name = f"Я не {name}, а Мария"
+
+person1 = Maria('Света')
+person2 = Maria('Мария')
+print(person1.name)
+print(person2.name)
+
+person2.surname = 'Макарова'
+
 
